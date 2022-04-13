667ae9b8e4c38dc7528676bb7ed2c4a04598860d
yoshuah809 <91335755+yoshuah809@users.noreply.github.com>
2022-04-13 13:55:30 -0500

    Converted App to Functional component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,41 @@
 import "./styles.css";
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: ""
-    };
-  }
+const App = () => {
+  const [monsters, setMonsters] = useState([]);
+  const [searchField, setSearchField] = useState("");
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => {
-            //console.log(this.state);
-          }
-        )
-      );
-  }
-
-  onSearchChange = (e) => {
-    const searchField = e.target.value;
-    this.setState(() => {
-      return { searchField };
-    });
+  const onSearchChange = (e) => {
+    const searchFieldstr = e.target.value;
+    setSearchField(searchFieldstr);
   };
 
-  render() {
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
+  const filteredMonsters = monsters.filter((monster) => {
+    return monster.name.toLowerCase().includes(searchField);
+  });
 
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(searchField);
-    });
+  const fetchData = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  };
 
-    return (
-      <div className="App">
-        <h1 className="app-title">Monsters Rolodex</h1>
-        <SearchBox onSearchChange={onSearchChange} />
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-        <CardList monsters={filteredMonsters} />
-        {/* <button className="btn btn-outline-primary">Change Name</button> */}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox onSearchChange={onSearchChange} />
+
+      <CardList monsters={filteredMonsters} />
+      {/* <button className="btn btn-outline-primary">Change Name</button> */}
+    </div>
+  );
+};
 
 export default App;
